0edae10f695b55726d6dc4da3a45a31312736d7c
vcpvanedev <vcp2403@gmail.com>
2025-06-01 20:18:36 -0300

    primer commit

---
+++ primerEntrega/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import routes from "./routes/index.js";
+
+const app =express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+// Rutas de la api
+app.use("/api", routes);
+
+// Ruta para manejar errores 404
+app.use((req, res) => {
+    res.status(404).json({ error: "Recurso no encontrado" });
+  });
+  
+  app.listen(8080, () => {
+    console.log("Servidor escuchando en el puerto 8080");
+  });
